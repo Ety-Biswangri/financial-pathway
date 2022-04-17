3e4402b6fe3e112fb6c4bc4b8b5f721a06a24f7a
Ety-Biswangri <etybgc02@gmail.com>
2022-04-18 03:50:55 +0600

    google sign in method added

--- src/components/Login/SocialLogin/SocialLogin.js
+++ src/components/Login/SocialLogin/SocialLogin.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle();
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -12,7 +31,7 @@ const SocialLogin = () => {
             </div>
 
             <div>
-                <button
+                <button onClick={handleGoogleSignIn}
                     className='btn w-50 d-flex justify-content-center align-items-center mx-auto my-2' style={{ backgroundColor: "#dab6fc", color: "black", fontWeight: "600", border: "none" }}>
                     <FcGoogle className='fs-3'></FcGoogle>
                     <span className='px-2'>Google Sign In</span>
