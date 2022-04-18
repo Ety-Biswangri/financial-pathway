018974200e185e7df9c358cd665824679aa07581
Ety-Biswangri <etybgc02@gmail.com>
2022-04-18 13:34:33 +0600

    not found page is added

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import notFound from '../../images/notFound.jpg';
 
 const PageNotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
+        <div className='w-100 mx-auto d-flex justify-content-center align-items-center ' style={{ minHeight: "80vh" }}>
+            <div className='d-flex justify-content-center align-items-center w-100'>
+                <img className=' w-50 fluid' src={notFound} alt="notFoundImage" />
+            </div>
         </div>
     );
 };
