4de3cac2b30df4a4699dadea94dce3415d5d4081
Ety-Biswangri <etybgc02@gmail.com>
2022-04-18 17:00:27 +0600

    talk to me button is added

--- src/components/Home/BannerPage/BannerPage.js
+++ src/components/Home/BannerPage/BannerPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Nav } from 'react-bootstrap';
 import "./BannerPage.css";
 import financial1 from "../../../images/financial1.jpg";
 import financial2 from "../../../images/financial2.jpg";
@@ -17,7 +17,8 @@ const BannerPage = () => {
                     <Carousel.Caption className='margin-bottom'>
                         <h3>MAKE A DIFFERENCE IN YOUR LIFE</h3>
                         <p> I help business owners and entrepreneurs to improve their relationship with money and stay on track to meet their financial goals.</p>
-                        <button style={{ color: "black", fontWeight: "700", border: "none" }}>Make an Appointment</button>
+
+                        <Nav.Link href="/home#talk"> <button style={{ color: "black", fontWeight: "700", border: "none" }}>Talk to Me</button> </Nav.Link>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -30,9 +31,8 @@ const BannerPage = () => {
                     <Carousel.Caption className='margin-bottom'>
                         <h3>MAKE YOUR PASSION COME TRUE </h3>
                         <p> I help others just like you to identify the problem areas or challenges in your finances and develop strategies to overcome them. </p>
-                        <button style={{ color: "black", fontWeight: "700", border: "none" }}>
-                            Make an Appointment
-                        </button>
+
+                        <Nav.Link href="/home#talk"> <button style={{ color: "black", fontWeight: "700", border: "none" }}>Talk to Me</button> </Nav.Link>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
